486735aec17be45fc9aff86b2b1b908090a95cc7
Mhayk Whandson <hi@mhayk.com>
2021-01-07 11:52:23 +0000

    Lesson 13 - Modals.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- index.js
+++ index.js
@@ -23,3 +23,17 @@ tabs.forEach((tab) => {
         })
     })
 })
+
+// modal
+
+const signupButton = document.querySelector('#signup');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+signupButton.addEventListener('click', () => {
+    modal.classList.add('is-active')
+})
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active')
+})
